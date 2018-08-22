6ea48758a514ec2f2ecf89a48b4902b1d8568bba
Markus <markus_er_nr3@hotmail.com>
2018-08-22 14:08:21 +0200

    Added prototype screen and image tools.

---
+++ img_tools.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageTk
+
+def blank_img(w, h, col = 'white'):
+    '''Creates blank (white) image of size w x h.
+    Assumes w, h to be integers
+    '''
+    img =  Image.new(mode = 'RGB', size = (w, h), color = col)
+    return ImageTk.PhotoImage(img)
