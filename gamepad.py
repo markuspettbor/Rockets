daebdcac5e70d9074b2e33ed04c1b77f3e9c2642
Markus <markus_er_nr3@hotmail.com>
2018-08-24 23:37:14 +0200

    updated viewscreen.py

--- gamepad.py
+++ gamepad.py
@@ -1,34 +1,8 @@
-import tkinter as tk
+import pygame as pg
 
 class Controller(object):
-    def __init__(self, master):
-        self.master = master
-        self.master.bind('<d>', self.dxr)
-        self.master.bind('<a>', self.dxl)
-        self.master.bind('<w>', self.dyu)
-        self.master.bind('<s>', self.dyd)
-        self.master.bind('<e>', self.rotatecw)
-        self.master.bind('<q>', self.rotateccw)
-
-    def dxr(self, event, direction = 'right'):
-        print('Turned right')
-
-    def dxl(self, event):
-        print('Turned left')
-
-    def dyu(self, event, direction = 'up'):
-        print('Went up')
-
-    def dyd(self, event):
-        print('Went down')
-
-    def rotatecw(self, event):
-        print('Turned clockwise')
-
-    def rotateccw(self, event):
-        print('Turned counter-clockwise')
+    def __init__(self):
+        pass
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    test = Controller(root)
-    root.mainloop()
+    pass
--- viewscreen.py
+++ viewscreen.py
@@ -1,9 +1,11 @@
 import pygame as pg
+import random
 
 class Screen(object):
     def __init__(self, w, h):
         self.w = w
         self.h = h
+        pg.init()
         #pg.display.init()
         pg.display.set_mode((self.w, self.h))
 
@@ -13,14 +15,38 @@ class Screen(object):
     def close(self):
         pg.display.quit()
 
+def test_draw_random_circle(surf):
+    radius = 0
+    x = random.randint(0, width- radius)
+    y = random.randint(0, height- radius)
+    pg.draw.circle(surf, (255,255,255,255), (x, y), radius)
+
+def test_timer(surf, txt):
+    font_name = pg.font.match_font('arial')
+    font = pg.font.Font(font_name, 70)
+    txt_surf = font.render(txt, True, (255, 255, 255, 255), (0,0,0,0))
+    txt_rect = txt_surf.get_rect()
+    txt_rect.midtop = (100, 40)
+    surf.blit(txt_surf, txt_rect)
+
 if __name__ == '__main__':
     width = 1920 # Can't remember resolution things
     height = 1080
     test = Screen(width, height)
-    running = True
-    while running:
+    testrun = True
+    clock = pg.time.Clock()
+    surf = pg.display.get_surface()
+    while testrun:
+        elapsed = pg.time.get_ticks()
+        time = clock.tick()
+        test_timer(surf, str(elapsed))
+        test_draw_random_circle(surf)
+        pg.display.flip()
         for event in pg.event.get():
-            print(event)
             if event.type == pg.QUIT:
                 test.close()
-                running = False
+                testrun = False
+
+        if elapsed > 10**4:
+            test.close()
+            testrun = False
