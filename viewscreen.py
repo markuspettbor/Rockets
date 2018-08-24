c1d7196528d5ac73f6415bc6093cadb9668948da
Markus <markus_er_nr3@hotmail.com>
2018-08-24 11:21:17 +0200

    Changed viewscreen.py to be pygame-based, instead of Tkinter.

--- viewscreen.py
+++ viewscreen.py
@@ -1,39 +1,26 @@
-import tkinter as tk
-import numpy as np
-import img_tools as it
-from PIL import Image, ImageTk, ImageDraw
-
-import time
-import random
+import pygame as pg
 
 class Screen(object):
-    def __init__(self, master, w, h):
-        self.master = master
+    def __init__(self, w, h):
         self.w = w
         self.h = h
-        self.set_initial()
+        #pg.display.init()
+        pg.display.set_mode((self.w, self.h))
 
-    def set_initial(self):
-        img = it.blank_img(self.w, self.h, 'white')
-        img  = ImageTk.PhotoImage(img)
-        self.frame = tk.Label(self.master, image  = img)
-        self.frame.image = img
-        self.frame.grid(column = 0, row = 0)
-
-    def add_image(self):
+    def update(self):
         pass
 
-    def update_image(self, img):
-        img = ImageTk.PhotoImage(img)
-        self.frame.configure(image = img)
-        self.frame.image = img
-        self.frame.grid(column = 0, row = 0)
-
-    def test_screen(self):
-        pass
-
+    def close(self):
+        pg.display.quit()
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    test = Screen(root, w = 1920, h = 1080)
-    root.mainloop()
+    width = 1920 # Can't remember resolution things
+    height = 1080
+    test = Screen(width, height)
+    running = True
+    while running:
+        for event in pg.event.get():
+            print(event)
+            if event.type == pg.QUIT:
+                test.close()
+                running = False
